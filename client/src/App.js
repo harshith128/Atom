a2cbd9d661087da568d8db5b61543dda07b18dfe
Shubham <shubh.110394@gmail.com>
2021-12-15 10:28:38 +0530

    routerAdded

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
-import './App.css';
-import LogoNav from './Components/LogoNavbar/LogoNav';
-import SignupPage from './Components/SignupPage/SignupPage';
+import "./App.css";
+import LogoNav from "./Components/LogoNavbar/LogoNav";
+import SignupPage from "./Components/SignupPage/SignupPage";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div>
-      <SignupPage />
-      <LogoNav></LogoNav>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Switch>
+				<Route exact path="/">
+					<LogoNav></LogoNav>
+				</Route>
+				<Route exact path="/signup">
+					<SignupPage />
+				</Route>
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
